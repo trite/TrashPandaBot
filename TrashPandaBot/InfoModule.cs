a53e7b3cbec244904fc2d292dd3b1140793ad8c5
Trite <pbrundog@gmail.com>
2021-09-01 08:48:44 -0700

    more stuff

--- TrashPandaBot/InfoModule.cs
+++ TrashPandaBot/InfoModule.cs
@@ -9,9 +9,20 @@ namespace TrashPandaBot
 {
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
+        public static int ChirpCount = 504;
+
         [Command("say")]
         [Summary("Echoes a message.")]
         public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
             => ReplyAsync(echo);
+
+        [Command("chirp")]
+        [Summary("Increments the Tide Chirp Counter™.")]
+        [Alias("chrip")]
+        public Task ChirpAsync()
+        {
+            ChirpCount++;
+            return ReplyAsync($"Tide Chirp Counter™: {ChirpCount}");
+        }
     }
 }
