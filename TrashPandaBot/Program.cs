9f19b8668533f3e18a3aa21b74305c5e7f3581bc
Trite <pbrundog@gmail.com>
2021-09-01 07:02:41 -0700

    initial commit

---
+++ TrashPandaBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace TrashPandaBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = "Need to store this.";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
